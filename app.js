457a18c701be87858e2df725090500b8419b05d2
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-07-09 20:17:41 -0400

    created forloop for titles to display from array

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const search = document.getElementById('search-books');
 const bookList = document.getElementById('book-list');
 
 console.log(search, bookList);
+
+var title = document.getElementsByClassName('title');
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+
+Array.from(title).forEach(function(item){
+    console.log(item);
+})
