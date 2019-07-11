9c11935007f7aae029c9c5b3c27da32f5afd676d
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-07-10 20:38:32 -0400

    queryselector/All to access elemets/child

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const search = document.getElementById('search-books');
-const bookList = document.getElementById('book-list');
+const wmf = document.querySelector('#book-list li:nth-child(2).name');
+console.log(wmf);
+var books = document.querySelector('#book-listi.name');
+//  console.log(books);
+books = document.querySelectorAll('#book-listi.name');
+// console.log(books);
+Array.from(books).forEach(function(book){
+        console.log(book);
 
-console.log(search, bookList);
-
-var title = document.getElementsByClassName('title');
-// console.log(Array.isArray(titles));
-// console.log(Array.isArray(Array.from(titles)));
-
-Array.from(title).forEach(function(item){
-    console.log(item);
 })
+
+// const search = document.getElementById('search-books');
+// const bookList = document.getElementById('book-list');
+
+// console.log(search, bookList);
+
+// var title = document.getElementsByClassName('title');
+// // console.log(Array.isArray(titles));
+// // console.log(Array.isArray(Array.from(titles)));
+
+// Array.from(title).forEach(function(item){
+//     console.log(item);
+// }
