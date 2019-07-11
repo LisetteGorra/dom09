f502c292d5e52975d813f353bb979b9c3727dab8
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-07-10 21:12:42 -0400

    appending inner html

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
-const wmf = document.querySelector('#book-list li:nth-child(2).name');
-console.log(wmf);
-var books = document.querySelector('#book-listi.name');
-//  console.log(books);
-books = document.querySelectorAll('#book-listi.name');
-// console.log(books);
+var books = document.querySelectorAll('book-list li.name');
+
 Array.from(books).forEach(function(book){
-        console.log(book);
+    book.textContent += '(book title)';
+});
 
-})
-
-// const search = document.getElementById('search-books');
-// const bookList = document.getElementById('book-list');
-
-// console.log(search, bookList);
-
-// var title = document.getElementsByClassName('title');
-// // console.log(Array.isArray(titles));
-// // console.log(Array.isArray(Array.from(titles)));
-
-// Array.from(title).forEach(function(item){
-//     console.log(item);
-// }
+const bookList = document.querySelector('#book-list');
+bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML</p>';
