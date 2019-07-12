38be996caa4677e48b985c897685ef0b8328b9ab
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-07-12 18:41:59 -0400

    traversing dom, child/parent nodes

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-var books = document.querySelectorAll('book-list li.name');
-
-Array.from(books).forEach(function(book){
-    book.textContent += '(book title)';
-});
-
 const bookList = document.querySelector('#book-list');
-bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML += '<p>This is how you add HTML</p>';
+
+console.log("the parent node is:", bookList.parentNode);
+console.log("the parent element is:", bookList.parentElement.parentElement);
+console.log(bookList.children);
+
+
+
+
+
+// var books = document.querySelectorAll('book-list li.name');
+
+// Array.from(books).forEach(function(book){
+//     book.textContent += '(book title)';
+// });
+
+// const bookList = document.querySelector('#book-list');
+// bookList.innerHTML = '<h2>Books and more books...</h2>';
+// bookList.innerHTML += '<p>This is how you add HTML</p>';
